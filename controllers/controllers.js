be138177ede9431f23af225518cfd1c7b04a88e9
thelasttrinh <aarontrinh57@yahoo.com>
2023-05-17 00:28:40 -0500

    Added CRUD, fixed db location

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -27,3 +27,46 @@ export const addProduct = (req, res) => {
     })
     .catch((err) => console.log(err));
 };
+
+export const removeProductById = (req, res) => {
+  const id = parseInt(req.params.id);
+  Product.destroy({
+    where: {
+      id: id,
+    },
+  })
+    .then(() => {
+      res.status(204).send({
+        message: `Product at id: ${id} deleted`,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+export const updateProduct = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  Product.update(
+    {
+      company: req.body.company,
+      name: req.body.name,
+      price: req.body.price,
+      type: req.body.type,
+      description: req.body.description,
+      quantity: req.body.quantity,
+    },
+    {
+      where: {
+        id: id,
+      },
+    }
+  )
+    .then(() => {
+      res
+        .status(200)
+        .send(`Product at ${id} updated` || `Product  at ${id} not found`);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
