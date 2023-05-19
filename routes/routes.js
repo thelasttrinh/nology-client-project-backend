834221858d220dcc23d588f94f73de1deff372b6
thelasttrinh <aarontrinh57@yahoo.com>
2023-05-19 15:58:37 -0500

    Improved README.md, fixed rating column, added filterByPrice, adjusted routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -27,30 +27,48 @@ import {
 
 const router = Router();
 
-router.get("/", getProducts);
+// router.get("/", getProducts);
 
+// router.get("/laptops", getLaptops);
+
+// router.get("/smartwatches", getSmartwatches);
+
+// router.get("/headphones", getHeadphones);
+
+// router.get("/speakers", getSpeakers);
+
+// router.post("/", addProduct);
+
+// router.delete("/:id", removeProductById);
+
+// router.put("/:id", updateProduct);
+
+// router.get("/cart", getCart);
+
+// router.post("/cart", addToCart);
+
+// router.delete("/cart/:id", deleteFromCart);
+
+// router.put("/cart/:id", updateCart);
+
+// router.get("/filterByPrice", filterByPrice);
+
+// router.get("/:id", getProductById);
+
+router.get("/filterByPrice", filterByPrice);
+router.get("/:id", getProductById);
 router.get("/laptops", getLaptops);
-
 router.get("/smartwatches", getSmartwatches);
-
 router.get("/headphones", getHeadphones);
-
 router.get("/speakers", getSpeakers);
-
-router.post("/", addProduct);
-
-router.delete("/:id", removeProductById);
-
-router.put("/:id", updateProduct);
-
 router.get("/cart", getCart);
 
+router.get("/", getProducts);
+router.post("/", addProduct);
+router.delete("/:id", removeProductById);
+router.put("/:id", updateProduct);
 router.post("/cart", addToCart);
-
 router.delete("/cart/:id", deleteFromCart);
-
 router.put("/cart/:id", updateCart);
-
-router.get("/:id", getProductById);
 
 export default router;
