5c928c0aa8b211c3059dcb265e543e8501342a9b
thelasttrinh <aarontrinh57@yahoo.com>
2023-05-24 11:23:41 -0500

    Changed order of routes to fix get cart issue

--- routes/routes.js
+++ routes/routes.js
@@ -53,6 +53,7 @@ const router = Router();
 
 // router.get("/:id", getProductById);
 
+router.get("/cart", getCart);
 router.get("/filterByPrice", filterByPrice);
 router.get("/filterByHighestRated", filterByHighestRated);
 router.get("/:id", getProductById);
@@ -60,7 +61,6 @@ router.get("/laptops", getLaptops);
 router.get("/smartwatches", getSmartwatches);
 router.get("/headphones", getHeadphones);
 router.get("/speakers", getSpeakers);
-router.get("/cart", getCart);
 
 router.get("/", getProducts);
 router.post("/", addProduct);
